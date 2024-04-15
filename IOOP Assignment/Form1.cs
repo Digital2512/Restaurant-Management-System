71cc0099d9912d5fec2448d1acd5e54161a628fa
Digital2512 <valerieannabella123@gmail.com>
2024-04-15 13:21:13 +0800

    intial commit

--- IOOP Assignment/Form1.cs
+++ IOOP Assignment/Form1.cs
@@ -16,10 +16,5 @@ namespace IOOP_Assignment
         {
             InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
