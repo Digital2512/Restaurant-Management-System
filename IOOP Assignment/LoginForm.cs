b09a5f3ba31d82d8f20d5f25cd0907685c9575c2
Digital2512 <valerieannabella123@gmail.com>
2024-04-23 21:12:47 +0800

    commit login system

---
+++ IOOP Assignment/LoginForm.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace IOOP_Assignment
+{
+    public partial class loginForm : Form
+    {
+        public loginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        //change the connection string when using a different laptop to connect to the database
+        public string connectionString = "Data Source=DESKTOP-9JG6P7V;Initial Catalog=IOOPDatabase;Integrated Security=True";
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            string username = txtUsername.Text.ToString();
+            string password = txtPassword.Text.ToString();
+
+            if(connection.State == System.Data.ConnectionState.Open)
+            {
+                try
+                {
+                    string query = $"SELECT * FROM Users WHERE UserID= @Username AND Password = @Password;";
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@Username", username);
+                    cmd.Parameters.AddWithValue("@Password", password);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.HasRows) 
+                    {
+                        reader.Close();
+                        string roleQuery = $"SELECT Role FROM Users WHERE UserID= @Username AND Password = @Password;";
+                        cmd = new SqlCommand(roleQuery, connection);
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        cmd.Parameters.AddWithValue("@Password", password);
+                        string role = cmd.ExecuteScalar() as string;
+                        if (role != null)
+                        {
+                            switch (role)
+                            {
+                                case "Admin":
+                                    MessageBox.Show("User's Role: Admin");
+                                    break;
+                                case "Manager":
+                                    MessageBox.Show("User's Role: Manager");
+                                    break;
+                                case "Chef":
+                                    MessageBox.Show("User's Role: Chef");
+                                    break;
+                                case "Customer":
+                                    MessageBox.Show("User's Role: Customer");
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Role not found");
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login Unsuccessful");
+                    }
+                        reader.Close();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("An Error Occured: " + ex);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Connection Unsuccessful");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+
