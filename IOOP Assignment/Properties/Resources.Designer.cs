d4e079601dcd0d71aac22ad1aefdca17cf6600ed
Digital2512 <valerieannabella123@gmail.com>
2024-05-21 14:13:49 +0800

    finalized design

--- IOOP Assignment/Properties/Resources.Designer.cs
+++ IOOP Assignment/Properties/Resources.Designer.cs
@@ -223,6 +223,16 @@ namespace IOOP_Assignment.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap profile_userResized {
+            get {
+                object obj = ResourceManager.GetObject("profile-userResized", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap recycleBinResized {
             get {
                 object obj = ResourceManager.GetObject("recycleBinResized", resourceCulture);
