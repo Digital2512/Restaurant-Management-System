d89b16a60d33a08a1c89aa16d9f7fe473240429b
hueyshin04 <148762211+hueyshin04@users.noreply.github.com>
2024-05-28 15:04:36 +0800

    commit admin

--- IOOP Assignment/Program.cs
+++ IOOP Assignment/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace IOOP_Assignment
+namespace trial_2
 {
     internal static class Program
     {
@@ -16,7 +16,7 @@ namespace IOOP_Assignment
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CustomerIndividualProductPage());
+            Application.Run(new AdminViewSalesReport());
         }
     }
 }
