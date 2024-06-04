751726e96fcc1085a5f890d6e1b20bad670795f5
Digital2512 <valerieannabella123@gmail.com>
2024-06-04 11:00:07 +0800

    commit manager

---
+++ IOOP Assignment/ManagerAddPlace.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace IOOP_Assignment
+{
+    partial class ManagerAddPlace
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picPlace = new System.Windows.Forms.PictureBox();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblPlaceID = new System.Windows.Forms.Label();
+            this.lblProDesc = new System.Windows.Forms.Label();
+            this.txtPlaceName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.txtPlaceID = new System.Windows.Forms.TextBox();
+            this.txtPax = new System.Windows.Forms.TextBox();
+            this.lblPax = new System.Windows.Forms.Label();
+            this.txtEventType = new System.Windows.Forms.TextBox();
+            this.lblEventType = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlace)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picPlace
+            // 
+            this.picPlace.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picPlace.Location = new System.Drawing.Point(44, 130);
+            this.picPlace.Name = "picPlace";
+            this.picPlace.Size = new System.Drawing.Size(140, 140);
+            this.picPlace.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPlace.TabIndex = 36;
+            this.picPlace.TabStop = false;
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(309, 113);
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(167, 26);
+            this.txtDesc.TabIndex = 37;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.PeachPuff;
+            this.btnAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdd.Location = new System.Drawing.Point(331, 283);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(85, 38);
+            this.btnAdd.TabIndex = 34;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblPlaceID
+            // 
+            this.lblPlaceID.AutoSize = true;
+            this.lblPlaceID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlaceID.Location = new System.Drawing.Point(226, 81);
+            this.lblPlaceID.Name = "lblPlaceID";
+            this.lblPlaceID.Size = new System.Drawing.Size(77, 22);
+            this.lblPlaceID.TabIndex = 29;
+            this.lblPlaceID.Text = "PlaceID:";
+            // 
+            // lblProDesc
+            // 
+            this.lblProDesc.AutoSize = true;
+            this.lblProDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProDesc.Location = new System.Drawing.Point(198, 113);
+            this.lblProDesc.Name = "lblProDesc";
+            this.lblProDesc.Size = new System.Drawing.Size(105, 22);
+            this.lblProDesc.TabIndex = 28;
+            this.lblProDesc.Text = "Description:";
+            // 
+            // txtPlaceName
+            // 
+            this.txtPlaceName.Location = new System.Drawing.Point(309, 154);
+            this.txtPlaceName.Name = "txtPlaceName";
+            this.txtPlaceName.Size = new System.Drawing.Size(167, 26);
+            this.txtPlaceName.TabIndex = 27;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(241, 155);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(62, 22);
+            this.lblName.TabIndex = 26;
+            this.lblName.Text = "Name:";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.BackColor = System.Drawing.Color.PeachPuff;
+            this.btnUpload.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUpload.Location = new System.Drawing.Point(71, 276);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(90, 38);
+            this.btnUpload.TabIndex = 25;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = false;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // txtPlaceID
+            // 
+            this.txtPlaceID.Location = new System.Drawing.Point(309, 81);
+            this.txtPlaceID.Name = "txtPlaceID";
+            this.txtPlaceID.Size = new System.Drawing.Size(117, 26);
+            this.txtPlaceID.TabIndex = 39;
+            // 
+            // txtPax
+            // 
+            this.txtPax.Location = new System.Drawing.Point(309, 195);
+            this.txtPax.Name = "txtPax";
+            this.txtPax.Size = new System.Drawing.Size(117, 26);
+            this.txtPax.TabIndex = 41;
+            // 
+            // lblPax
+            // 
+            this.lblPax.AutoSize = true;
+            this.lblPax.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPax.Location = new System.Drawing.Point(210, 199);
+            this.lblPax.Name = "lblPax";
+            this.lblPax.Size = new System.Drawing.Size(93, 22);
+            this.lblPax.TabIndex = 40;
+            this.lblPax.Text = "MinOfPax:";
+            // 
+            // txtEventType
+            // 
+            this.txtEventType.Location = new System.Drawing.Point(309, 227);
+            this.txtEventType.Name = "txtEventType";
+            this.txtEventType.Size = new System.Drawing.Size(117, 26);
+            this.txtEventType.TabIndex = 43;
+            // 
+            // lblEventType
+            // 
+            this.lblEventType.AutoSize = true;
+            this.lblEventType.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEventType.Location = new System.Drawing.Point(201, 227);
+            this.lblEventType.Name = "lblEventType";
+            this.lblEventType.Size = new System.Drawing.Size(102, 22);
+            this.lblEventType.TabIndex = 42;
+            this.lblEventType.Text = "EventType:";
+            // 
+            // AddPlace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtEventType);
+            this.Controls.Add(this.lblEventType);
+            this.Controls.Add(this.txtPax);
+            this.Controls.Add(this.lblPax);
+            this.Controls.Add(this.txtPlaceID);
+            this.Controls.Add(this.picPlace);
+            this.Controls.Add(this.txtDesc);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblPlaceID);
+            this.Controls.Add(this.lblProDesc);
+            this.Controls.Add(this.txtPlaceName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnUpload);
+            this.Name = "AddPlace";
+            this.Text = "AddPlace";
+            ((System.ComponentModel.ISupportInitialize)(this.picPlace)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPlace;
+        private System.Windows.Forms.TextBox txtDesc;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblPlaceID;
+        private System.Windows.Forms.Label lblProDesc;
+        private System.Windows.Forms.TextBox txtPlaceName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.TextBox txtPlaceID;
+        private System.Windows.Forms.TextBox txtPax;
+        private System.Windows.Forms.Label lblPax;
+        private System.Windows.Forms.TextBox txtEventType;
+        private System.Windows.Forms.Label lblEventType;
+    }
+}
--- IOOP Assignment/ManagerHomePage.Designer.cs
+++ IOOP Assignment/ManagerHomePage.Designer.cs
@@ -28,12 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblGreetMng = new System.Windows.Forms.Label();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.lblResv = new System.Windows.Forms.Label();
+            this.btnResvReport = new System.Windows.Forms.Button();
+            this.btnTables = new System.Windows.Forms.Button();
+            this.btnMenu = new System.Windows.Forms.Button();
+            this.picManager = new System.Windows.Forms.PictureBox();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picManager)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblGreetMng
+            // 
+            this.lblGreetMng.AutoSize = true;
+            this.lblGreetMng.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGreetMng.Location = new System.Drawing.Point(542, 204);
+            this.lblGreetMng.Name = "lblGreetMng";
+            this.lblGreetMng.Size = new System.Drawing.Size(158, 25);
+            this.lblGreetMng.TabIndex = 2;
+            this.lblGreetMng.Text = "Hello, Manager";
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(461, 236);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(245, 25);
+            this.lblQuestion.TabIndex = 3;
+            this.lblQuestion.Text = "What would you like to do?";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.picLogo);
+            this.panel1.Location = new System.Drawing.Point(237, 48);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(248, 213);
+            this.panel1.TabIndex = 17;
+            // 
+            // picLogo
+            // 
+            this.picLogo.BackColor = System.Drawing.Color.Transparent;
+            this.picLogo.Location = new System.Drawing.Point(-12, -15);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(269, 231);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLogo.TabIndex = 21;
+            this.picLogo.TabStop = false;
+            // 
+            // lblResv
+            // 
+            this.lblResv.AutoSize = true;
+            this.lblResv.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResv.Location = new System.Drawing.Point(36, 534);
+            this.lblResv.Name = "lblResv";
+            this.lblResv.Size = new System.Drawing.Size(165, 20);
+            this.lblResv.TabIndex = 9;
+            this.lblResv.Text = "Reservation Report";
+            // 
+            // btnResvReport
+            // 
+            this.btnResvReport.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnResvReport.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnResvReport.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnResvReport.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnResvReport.Location = new System.Drawing.Point(0, 388);
+            this.btnResvReport.Name = "btnResvReport";
+            this.btnResvReport.Size = new System.Drawing.Size(743, 50);
+            this.btnResvReport.TabIndex = 18;
+            this.btnResvReport.Text = "Reservation Report";
+            this.btnResvReport.UseVisualStyleBackColor = false;
+            this.btnResvReport.Click += new System.EventHandler(this.btnResvReport_Click);
+            // 
+            // btnTables
+            // 
+            this.btnTables.BackColor = System.Drawing.Color.LightGray;
+            this.btnTables.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTables.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnTables.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnTables.Location = new System.Drawing.Point(0, 338);
+            this.btnTables.Name = "btnTables";
+            this.btnTables.Size = new System.Drawing.Size(743, 50);
+            this.btnTables.TabIndex = 19;
+            this.btnTables.Text = "Tables";
+            this.btnTables.UseVisualStyleBackColor = false;
+            this.btnTables.Click += new System.EventHandler(this.btnTables_Click);
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.BackColor = System.Drawing.Color.DarkGray;
+            this.btnMenu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMenu.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnMenu.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMenu.Location = new System.Drawing.Point(0, 288);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(743, 50);
+            this.btnMenu.TabIndex = 20;
+            this.btnMenu.Text = "Menu";
+            this.btnMenu.UseVisualStyleBackColor = false;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // picManager
+            // 
+            this.picManager.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picManager.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picManager.Location = new System.Drawing.Point(554, 30);
+            this.picManager.Name = "picManager";
+            this.picManager.Size = new System.Drawing.Size(146, 140);
+            this.picManager.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picManager.TabIndex = 15;
+            this.picManager.TabStop = false;
+            this.picManager.Click += new System.EventHandler(this.picManager_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(22, 12);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(32, 32);
+            this.btnLogout.TabIndex = 21;
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // ManagerHomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(743, 438);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.btnMenu);
+            this.Controls.Add(this.btnTables);
+            this.Controls.Add(this.btnResvReport);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.picManager);
+            this.Controls.Add(this.lblResv);
+            this.Controls.Add(this.lblGreetMng);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ManagerHomePage";
             this.Text = "ManagerHomePage";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picManager)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblGreetMng;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblResv;
+        private System.Windows.Forms.PictureBox picManager;
+        private System.Windows.Forms.Button btnResvReport;
+        private System.Windows.Forms.Button btnTables;
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.Button btnMenu;
+        private System.Windows.Forms.Button btnLogout;
     }
 }
